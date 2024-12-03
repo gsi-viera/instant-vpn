9731942a5d5d86d384d397dcf41998b5707cc6ed
Ariel Viera <viera@generalsoftwareinc.com>
2024-12-03 17:03:04 -0500

    added simple config endpoint

--- apps/ivpn-server/cmd/server/server.go
+++ apps/ivpn-server/cmd/server/server.go
@@ -7,5 +7,12 @@ func main() {
 	r.GET("/status", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "online"})
 	})
+
+	r.GET("/config", func(c *gin.Context) {
+		key := c.Query("key")
+		c.JSON(200, gin.H{
+			"receivedKey": key,
+		})
+	})
 	r.Run()
 }
